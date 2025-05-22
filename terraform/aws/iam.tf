6111df45d656fd74722f9baa1b9d6ad2f0b47a99
Christopher Caldwell <cudgel@users.noreply.github.com>
2025-05-22 12:51:53 -0400

    update s3 access role

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -48,6 +48,41 @@ resource "aws_iam_role_policy" "kms_decrypt_policy" {
   })
 }
 
+# Create an inline policy for SSM and EC2 messages
+resource "aws_iam_role_policy" "ssm_policy" {
+  name = "ssm-policy"
+  role = aws_iam_role.s3_access.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:ListInstanceAssociations",
+          "ssm:PutComplianceItems",
+          "ssm:UpdateInstanceAssociationStatus",
+          "ssm:UpdateInstanceInformation",
+          "ec2messages:AcknowledgeMessage",
+          "ec2messages:DeleteMessage",
+          "ec2messages:FailMessage",
+          "ec2messages:GetEndpoint",
+          "ec2messages:GetMessages",
+          "ec2messages:SendReply",
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+          "s3:Get*",
+          "s3:List*",
+          "s3:Describe*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "s3_access" {
   name = "ar-${var.general.key_name}-${var.general.attack_range_name}-instance-profile"
   path = "/"
