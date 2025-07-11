be0f8cb0cb2bb3681c321b9700615c10eaec51be
Christopher Caldwell <cudgel@users.noreply.github.com>
2025-07-11 07:12:55 -0400

    Update Kali AMI to use latest snapshot

--- terraform/aws/modules/kali-server/resources.tf
+++ terraform/aws/modules/kali-server/resources.tf
@@ -6,7 +6,7 @@ data "aws_ami" "latest-kali-linux" {
 
   filter {
     name   = "name"
-    values = ["kali-last-snapshot-amd64-2024*"]
+    values = ["kali-last-snapshot-amd64-*"]
   }
 
   filter {
